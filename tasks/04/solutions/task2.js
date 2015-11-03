467214d74d160847969bac2803dde91052f3cd6b
Ilia Idakiev <iliaidakiev@Digiral-Hustla.local>
2015-11-03 11:22:37 +0200

    fixed some typos

--- tasks/04/solutions/task2.js
+++ tasks/04/solutions/task2.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
 //In this solution we can create a function promisify which takes a function as an argument 
-//and retuns a promise but I will stick to the more generic way
-//PS. On github there are modules that provide this functionality
+//and retuns a promise but I will stick to the more generic way.
+//PS. On github there are modules that provide this functionality.
 
-//Because we dont have an async operation here the better solution to return a resolved Promise 
-//instead of creating a new promise like readFile and writeFile and resolving it inside
+//Because we dont have an async operation here the better solution is to return a resolved Promise 
+//instead of creating a new promise like readFile and writeFile and resolved it inside.
 function getRandomString(data) {
     data = data || {}; //if data is undefined or null assign an empry object
     data.string = Math.random().toString(36).substring(7);
@@ -21,14 +21,14 @@ function wait1000(data) {
         console.log('simulating async operation...');
 
         //Its always a good idea to name our functions for debuging purposes.
-        //This way in the stack trace we can see the function name instad of anonymous function
+        //This way in the stack trace we can see the function name instad of anonymous function.
         setTimeout(function asyncOperation(){
             resolve(data);
         }, 1000);
     });
 }
 
-//I will curry the writeFile and readFile Functions so I can pass the necessary paths when I chain the promises
+//I will curry the writeFile and readFile Functions so I can pass the necessary paths when I chain the promises.
 function writeFile(path) {
     return function writeFilePromise(data) {
         return new Promise(function(resolve, reject){
@@ -46,7 +46,7 @@ function concatContents(data){
     return Promise.resolve(data);
 }
 
-//We will store the contents of the files in an array - contents
+//We will store the contents of the files in an array - contents.
 function readFile(path) {
     return function readFilePromise(data) {
         return new Promise(function(resolve, reject) {
@@ -72,8 +72,8 @@ function doStuff() {
     };
 
     wait1000(data)
-        .then(getRandomString) //then expects a function as arguments .then(onFulfilled, onRejected)
-        .then(writeFile(FILE1_PATH)) //Because readFile and writeFile are curried we can pass an argument
+        .then(getRandomString) //Then expects a function as arguments .then(onFulfilled, onRejected).
+        .then(writeFile(FILE1_PATH)) //Because readFile and writeFile are curried we can pass an argument.
         .then(wait1000)
         .then(getRandomString)
         .then(writeFile(FILE2_PATH))
@@ -84,7 +84,7 @@ function doStuff() {
         .then(wait1000)
         .then(concatContents)
         .then(writeFile(FILE3_PATH))
-        .catch(handleError) //this will catch an error from any of the above functions
+        .catch(handleError) //handleError will catch an error from any of the above functions.
         .then(function finalMessage(data) {
             console.log('Thats much better...');
         });
